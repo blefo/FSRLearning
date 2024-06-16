04e92e7ed826b27050f46960f3460061c6b377b8
JayTicku <147661917+JayTicku@users.noreply.github.com>
2024-06-16 16:50:43 -0500

    Update __init__.py

--- FSRLearning/__init__.py
+++ FSRLearning/__init__.py
@@ -1,6 +1,6 @@
-from FSRLearning.action import Action
-from FSRLearning.state import State
-from FSRLearning.feature_selector_V3 import FeatureSelectorRL
+from .action import Action
+from .state import State
+from .feature_selector_V3 import FeatureSelectorRL
 
 __all__ = [
     'Action',
